674c3895e492f2c116443728acc40468a9ca940f
김윤기 <kalsagansteve@gmail.com>
2026-09-04 17:17:23 +0900

    TODO 41

--- src/main/java/com/example/study/application/ApplicationRepository.java
+++ src/main/java/com/example/study/application/ApplicationRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +24,8 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
      * 반환형태    List<Application>
      * 동작결과    신청 목록에서 신청자 별명이 조회 하나로 나옴
      */
-
+    @EntityGraph(attributePaths = {"applicant"})
+    List<Application> findWithApplicantByStudyPostIdOrderByCreatedAtAsc(Long studyPostId);
     /**
      * 내 신청 목록.
      *
